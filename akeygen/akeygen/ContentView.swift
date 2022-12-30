b8f4d0211f5425a219036ae633b7f93da5691381
Gladosator <65889266+Gladosator@users.noreply.github.com>
2022-12-30 10:40:35 +0100

    The algorythm now does revalidation of the string just like in the original

--- akeygen/akeygen/ContentView.swift
+++ akeygen/akeygen/ContentView.swift
@@ -118,76 +118,32 @@ struct ContentView: View {
         }
     }
     
-    func generateKeyFromName() {
-        /*
-         var ret = "";
-            name = name.toUpperCase();
-            var nameRecalc = name;
-            
-            if (name.length >= 15){
-                console.log("Name too long");
-                return "#";
-            }
-            for (var i = 0; i < name.length; i++){
-                var a = name.charAt(i);
-                if ((a < 'A' || a > 'Z') && a != "_" && (a < '0' || a > '9')){
-                    console.log("Invalid character, use only A to Z uppercase");
-                    return "#";
-                }
-            }
-            
-            var encName = "";
-            var writtenBytes = 0;
-            for (var i = 0; i < name.length; i++){
-                encName += (70 - (26 - (name.charCodeAt(i) - 'A'.charCodeAt(0)))).toString();
-                writtenBytes++;
-            }
-            encName += (EndBytes[Math.floor(Math.random() * EndBytes.length)]).toString();
-            writtenBytes++;
-            console.log("Encoded name as: " + encName);
-            
-            var fullNameStr = encName;
-            
-            while (writtenBytes != 15){
-                fullNameStr += (10+Math.floor(Math.random() * 89)).toString();
-                writtenBytes++;
-            }
-            
-            var checksumFullName = 0;
-            for (var i = 0; i < fullNameStr.length; i++){
-                checksumFullName += (fullNameStr.charCodeAt(i) - '0'.charCodeAt(0));
-            }
-            var checksumName = 0;
-            for (var i = 0; i < encName.length; i++){
-                checksumName += (encName.charCodeAt(i) - '0'.charCodeAt(0));
-            }
-            checksumName %= 100;
-            
-            console.log("checksum of full name: " + checksumFullName);
-            var checkSumPart1 = checksumFullName + Math.floor(Math.random() * (999-checksumFullName));
-            var checkSumPart2 = checkSumPart1 - checksumFullName;
-            
-            var retStr = "";
-            retStr += ('000'+checkSumPart1).slice(-3);
-            retStr = retStr.split("").reverse().join("");
-            retStr += fullNameStr;
-            retStr += ('000'+checkSumPart2).slice(-3);
-            retStr += ('00'+checksumName).slice(-2);
-
-            retStr = retStr.slice(0, 6) + "-" + retStr.slice(6);
-            retStr = retStr.slice(0, 15) + "-" + retStr.slice(15);
-            retStr = retStr.slice(0, 23) + "-" + retStr.slice(23);
-            retStr = retStr.slice(0, 31) + "-" + retStr.slice(31);
-            retStr = retStr.slice(0, 36) + "-" + retStr.slice(36);
-
-            return retStr;
-         */
+    func generateKeyFromName() -> String {
         log += "generating key for \"\(fullName)\" with end bytes \(endBytes.map({" \($0) "}))\n"
 
         if validate(fullName) {
             key = String(data:encode(fullName),
                          encoding:.utf8) ?? ""
         }
+        var ret:String = ""
+        var name:String = fullName.uppercased()
+        var nameRecalc:String = name
+        if name.count >= 15 {
+            print("Name too long")
+            return "#"
+        }
+        var length:Int = name.count - 1
+        let CharSet = CharacterSet(charactersIn: "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        if name.rangeOfCharacter(from: CharSet.inverted) != nil {
+            print("Invalid character, use only A to Z uppercase")
+            return "#"
+        }
+        var encName:String
+        var temp:DynamicProperty
+        var writtenBytes:Int
+        for i in 0 ... length {
+        }
+        return ret
     }
 }
 
