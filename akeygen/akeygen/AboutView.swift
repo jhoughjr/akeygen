2e34a2d01c9f133ed4c6cfcdef2bc188a45976b7
Gladosator <65889266+Gladosator@users.noreply.github.com>
2023-02-03 08:13:00 +0100

    ok works now...

--- akeygen/akeygen/AboutView.swift
+++ akeygen/akeygen/AboutView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct AboutView: View {
+    @State private var CrtY: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("AppNFO")
+            Text("Akeygen")
+                .font(.custom("Minecraft-bold",size: 30))
+            Divider()
+            Text("Made by Gladosator and Jimmy")
+                .font(.custom("Minecraft", size: 15))
+            Text("© Alex Vysokai \(String(format:"%04d",CrtY)), All rights reserved")
+        }
+        .onAppear() {
+            let calendar = Calendar.current
+            CrtY = calendar.component(.year, from: Date())
+
+
+        }
+        .frame(minWidth: 400, minHeight: 400)
     }
 }
 
